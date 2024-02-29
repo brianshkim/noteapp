373b226f4ec72374265eca381504a46fae96fb96
brianshkim <brianshkim25@gmail.com>
2024-02-29 01:40:52 -0800

    authentication and splash page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,18 @@
-import { Button } from "@/components/ui/button";
 
-export default function Home() {
+import { Heroes } from "./_components/heroes";
+import { Footer } from "./_components/footer";
+import { Heading } from "./_components/heading";
+
+const MarketingPage = () => {
   return (
-    <div>
-    <Button variant="outline">
-      Delete
-      </Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-cent er gap-y-8 flex-1 px-6 pb-10">
+        <Heading/>
+        <Heroes />
       </div>
+      <Footer />
+    </div>
   );
-}
+};
+
+export default MarketingPage;
