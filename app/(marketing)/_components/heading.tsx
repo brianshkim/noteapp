880edc8849167d7923db3e2801a2e3e95d4cd7e2
brianshkim <brianshkim25@gmail.com>
2024-03-01 04:18:18 -0800

    navbar

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,20 +1,44 @@
-"use client"
-import { ArrowRight } from "lucide-react"
-import { Button } from "@/components/ui/button"
+"use client";
+import { ArrowRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import { SignInButton } from "@clerk/clerk-react";
+import Link from "next/link";
 
-export const Heading = () =>{
-    return (
-        <div className="max-w-3xl space-y-4">
-            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-                Your space <span className="underline">vBlue</span>
-            </h1>
-            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-                vBlue is your own personal workspace.
-            </h3>
-            <Button>
-                Enter vBlue
-                <ArrowRight className="h-4 w-4 ml-2"/>
-            </Button>
+export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your space <span className="underline">vBlue</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        vBlue is your own personal workspace.
+      </h3>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
         </div>
-    )
-}
+      )}
+      {!isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter vBlue
+          </Link>
+          <ArrowRight className="h-4 w-4 ml-2" />
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton>
+            <Button>
+                Get vBlue
+                <ArrowRight className="h-4 w-4 ml-2" />
+            </Button>
+
+        </SignInButton>
+      )}
+    </div>
+  );
+};
